959be9208deb54523d0e28ef59f4056ae3bf69f6
miss jody <missjodyrussell@gmail.com>
2021-04-30 17:55:50 -0600

    city, weather icon and temp all dynamically update on fetchedCity

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -24,53 +24,59 @@ import {Jumbotron, Container, Row, Col} from "react-bootstrap"
 
 const Header = () => {
 
-    const [city, setCity] = useState("Denver, CO")
+    // const [city, setCity] = useState("Denver, CO")
     const [lat, setLat] = useState(39.59)
     const [lon, setLon] = useState(-104.97)
     const [fetchedCity, setFetchedCity] = useState([])
+    // const [fetchedWeather, setFetchedWeather] = useState([])
+    const [fetchedIcon, setFetchedIcon] = useState("")
+    const [fetchedTemp, setFetchedTemp] = useState(0)
     let fetchCityLink = ""
+    let weatherLink = ""
+    let fetchedIconLink = "test"
     
     useEffect(() => {
 
         // use if it is available or not to decide what to give CurCity/set state to
         if ("geolocation" in navigator) {
             // console.log("Available");
+
             navigator.geolocation.getCurrentPosition(function(position) {
+                // console.log("Called information, " + lat + ", " + lon + ", " + city);
                 // console.log("Latitude is :", position.coords.latitude.toFixed(2));
-                setLat(position.coords.latitude.toFixed(2))
                 // console.log("Longitude is :", position.coords.longitude.toFixed(2));
+
+                setLat(position.coords.latitude.toFixed(2))
                 setLon(position.coords.longitude.toFixed(2))
-                console.log("Called information, " + lat + ", " + lon + ", " + city);
+
             });
 
             fetchCityLink = "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=" + lat + "&longitude=" + lon + "&localityLanguage=en";
+            weatherLink = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat +"&lon=" + lon + "&appid=" + process.env.REACT_APP_API_KEY;
             
-            // Do call for the api to set the new city
-            fetch(fetchCityLink) 
-                .then(res => res.json())
-                .then((result) => {
-                    setFetchedCity(result.city + ", " + result.principalSubdivisionCode)
-                    console.log("Did it work? ", fetchedCity)
-                // },
-                // (error) => {
-                //    setFetchedCity(error)
-                // });
-                    }
-                )
-
-            // Do call for the new lat/lon for weather information
-            // set it to a variable? or a state?
+            Promise.all([
+                fetch(fetchCityLink),
+                fetch(weatherLink)
+            ])
+            .then(([res1, res2]) => Promise.all([res1.json(), res2.json()]))
+            .then(([data1, data2]) => {
+                setFetchedCity(data1.city + ", " + data1.principalSubdivisionCode)
+                setFetchedIcon(data2.current.weather[0].icon)
+                let Ktemp =  ((data2.current.temp - 273) * (9/5) + 32);
+                let Ftemp = Math.floor(Ktemp);
+                setFetchedTemp(Ftemp);
+            })
             
-          } else {
-            console.log("Use Default, " + lat + ", " + lon + ", " + city);
-
-            // Do call for the lat/lon for weather information
-            // set it to a variable? or a state?
+            
+            
             
         }
-
+        
+        
     })
-
+    
+    fetchedIconLink = "http://openweathermap.org/img/wn/" + fetchedIcon + "@2x.png"
+    console.log("is the img broken?? ", fetchedIconLink) // it's not btw
 
     return (
         <Jumbotron fluid>
@@ -82,7 +88,7 @@ const Header = () => {
                     <Col> {fetchedCity} </Col>
                     <Col> WeatherDash Logo Goes Here </Col>
                     {/* change image and temp over to call icon information */}
-                    <Col> <img alt="weatherIcon" src="http://openweathermap.org/img/wn/10d@2x.png"/> and Current Temp </Col>
+                    <Col> <img alt="weatherIcon" src={fetchedIconLink}/> {fetchedTemp}° </Col>
                 </Row>
             </Container>
         </Jumbotron>
@@ -91,3 +97,37 @@ const Header = () => {
   
   
   export default Header;
+
+
+
+     // Do call for the api to set the new city
+            // fetch(fetchCityLink) 
+            //     .then(res => res.json())
+            //     .then((result) => {
+            //         // console.log("Did it work? ", fetchedCity)
+         
+            //         setFetchedCity(result.city + ", " + result.principalSubdivisionCode)
+
+            //     })
+                // Do call for the new lat/lon for weather information
+                // set it to a variable? or a state? -- state, for reasons
+                // does this need to be called in a line with the last fetch -- yes otherwise it gets skipped
+                // .then(fetch(weatherLink)
+                // .then(res2 => res2.json())
+                // .then((result2) => {
+                //     // console.log("Did weather fetch work? ", result.current.temp)
+                //     setFetchedIcon(result2.current.weather[0].icon)
+                //     let Ktemp =  ((result2.current.temp - 273) * (9/5) + 32);
+                //     let Ftemp = Math.floor(Ktemp);
+                //     setFetchedTemp(Ftemp);
+                // }).then(
+                //     fetchedIconLink = "http://openweathermap.org/img/wn/" + fetchedIcon + "@2x.png"
+                // )
+                
+            
+            //   } else {
+                
+                // console.log("Use Default, " + lat + ", " + lon + ", " + city);
+                
+                // Do call for the lat/lon for weather information
+                // set it to a variable? or a state?
